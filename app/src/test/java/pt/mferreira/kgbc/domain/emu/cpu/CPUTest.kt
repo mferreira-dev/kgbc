98617dd61792746c9fe912523f8dd6f8c98a1239
mferreira-dev <mfkf1997@gmail.com>
2022-11-06 03:47:45 +0000

    Added even more tests.

--- app/src/test/java/pt/mferreira/kgbc/domain/emu/cpu/CPUTest.kt
+++ app/src/test/java/pt/mferreira/kgbc/domain/emu/cpu/CPUTest.kt
@@ -4,6 +4,9 @@ import org.junit.Assert.assertTrue
 import org.junit.Test
 import pt.mferreira.kgbc.domain.emu.cpu.CPUConstants.SUB_MASK
 import pt.mferreira.kgbc.domain.emu.entities.RefUByte
+import pt.mferreira.kgbc.domain.emu.entities.RefUShort
+import pt.mferreira.kgbc.utils.convertToBin
+import pt.mferreira.kgbc.utils.shl
 import pt.mferreira.kgbc.utils.shr
 
 class CPUTest {
@@ -15,10 +18,24 @@ class CPUTest {
 	private val reg = Array(CPUConstants.NUMBER_OF_8_BIT_REGISTERS) { RefUByte() }
 
 	@Test
+	fun conjoinRegisters() {
+		// Test code.
+		val msb = RefUByte(100u)
+		val lsb = RefUByte(248u)
+
+		val conjoined = RefUShort().apply {
+			value = value or RefUShort(msb.value.toUShort() shl 8).value
+			value = value or RefUShort(lsb.value.toUShort()).value
+		}
+
+		assertTrue(conjoined.value.convertToBin() == "0110010011111000")
+	}
+
+	@Test
 	fun maskOpcode() {
 		var isMatch = true
 
-		// Test.
+		// Test code.
 		val mask = SUB_MASK
 		val target = "10010001"
 
@@ -39,7 +56,7 @@ class CPUTest {
 	fun setZeroFlagTrue() {
 		reg[F_IDX].value = (reg[F_IDX].value.toInt() or 0x80).toUByte()
 
-		// Test.
+		// Test code.
 		val zeroFlagValue = (reg[F_IDX].value.toInt() and 0x80).toUByte()
 		assertTrue((zeroFlagValue shr 7).toInt() == 0x1)
 	}
@@ -48,7 +65,7 @@ class CPUTest {
 	fun setZeroFlagFalse() {
 		reg[F_IDX].value = (reg[F_IDX].value.toInt() and 0x70).toUByte()
 
-		// Test.
+		// Test code.
 		val zeroFlagValue = (reg[F_IDX].value.toInt() and 0x80).toUByte()
 		assertTrue((zeroFlagValue shr 7).toInt() == 0x0)
 	}
